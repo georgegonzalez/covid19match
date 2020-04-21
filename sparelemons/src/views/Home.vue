b33fe789b33a58b50fcccfccf6cff2f250caa925
George Gonzalez <ggonzalez@teamexos.com>
2020-04-20 23:03:03 -0700

    Initial VUE push.

---
+++ sparelemons/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Spare Lemons</h1>
+    <img src="../assets/1F34B.png" width="400px" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ sparelemons/src/views/Privacy.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="about">
+        <h1>Privacy Policy</h1>
+        <div class="policy">
+            <h3>Nothing</h3>
+            <p>None of your personal information is collected.<br />
+                Therefore, none of your data is stored.<br />
+                Consequently, none of your data is sold by Spare Lemons.</p>
+
+            <p>Seriously. We don't need that kind of negativity in our lives.</p>
+            <p>Relax, make some lemonade.</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.policy {
+    text-align: left;
+    margin: 2em 20%;
+
+    font-size: 1.5em;
+}
+</style>
